f3f7b22d474e52b5dd674998f1af141baa34a29b
MParrillaL <manuelparrillalahoz@gmail.com>
2026-03-04 18:40:23 +0100

    animación moneda +  correcciones

--- escenas/moneda/moneda.gd
+++ escenas/moneda/moneda.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @export var area_2d: Area2D
+@onready var anim = $AnimatedSprite2D
 
 func _ready() -> void:
 	area_2d.body_entered.connect(_recogida)
+	anim.play("default")
 
 func _recogida(body):
 	if body.is_in_group("jugador"): # ← asegúrate de que dusty está en este grupo
